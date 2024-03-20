729d780b1d81b3ae3b732157dffb9aa7ed77cae8
ColtonCurtis <imcoltoncurtis@gmail.com>
2024-03-19 20:29:54 -0600

    reduced video files sizes

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,3 @@
-import about04 from '../assets/about04.png';
-
 import exit from '../assets/exit.png';
 
 import dropDownTriangle from '../assets/dropDownTriangle.png';
@@ -8,16 +6,12 @@ import angledArrow from '../assets/angledArrow.png';
 
 import gitHubLogo from '../assets/github-mark-white.png';
 
-import coltonProfile from '../assets/coltonProfile.png';
-
 const images = {
-  about04,
   exit,
   dropDownTriangle,
   envelopeIcon,
   angledArrow,
   gitHubLogo,
-  coltonProfile,
 };
 
 export default images;
